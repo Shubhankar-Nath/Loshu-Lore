12cf964e247f4b594452572a1374b87005088f65
Nath, Shubhankar <snath38@gatech.edu>
2024-04-25 17:51:49 +0530

    Inital Setup

---
+++ notebookscripts.py
@@ -0,0 +1,30 @@
+import datetime
+import fpdf
+
+class PDF_Utils:
+
+    WIDTH = 210
+    HEIGHT = 297
+    def generate_front_page(username, dob):
+        pdf = fpdf.FPDF()
+        pdf.add_page()
+        pdf.set_font("Arial", size = 26)
+        pdf.ln(85)
+        pdf.write(5, "Your Numerology Guide")
+        pdf.set_font("Calibri", size = 12)
+        pdf.ln(15)
+        pdf.write(5, "Report For: " + username)
+        pdf.ln(10)
+        pdf.write(5, "Date of Birth: " + dob)
+        pdf.ln(10)
+        pdf.write(5, "Report generated on: " + str(datetime.datetime.now()))
+        return pdf
+
+    def add_page(pdf):
+        pdf.add_page()
+        #code to add a footer to the page
+        pdf.image("./assets/logo no text.jpg", 0, 280, WIDTH)
+
+
+    def save_pdf(filename, pdf):
+        pdf.output("filename"+'_report.pdf')
