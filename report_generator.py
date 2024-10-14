7bb7084b2a34fe19a459b544a48c27c71e9b30e3
Nath, Shubhankar <snath38@gatech.edu>
2024-10-14 18:55:37 +0530

    Successful file saving

--- report_generator.py
+++ report_generator.py
@@ -12,8 +12,10 @@ LO_SHU_GRID = [[4,9,2],[3,5,7],[8,1,6]]
 # Main Code (Do not Edit the block of code below)
 if __name__ == "__main__":
     file_util_agent = FILE_Utils()
+    pdf_util_agent = PDF_Utils()
+    
     input_json = file_util_agent.read_input_file()
     if file_util_agent.validate_input_file(input_json) is False:
         exit()
-    doc = PDF_Utils.add_cover(file_util_agent.get_fullname(input_json), file_util_agent.get_fulldate(input_json))
-    PDF_Utils.save_pdf(file_util_agent.get_fullname(input_json), doc)
+    doc = pdf_util_agent.add_cover(file_util_agent.get_fullname(input_json), file_util_agent.get_fulldate(input_json))
+    pdf_util_agent.save_pdf(file_util_agent.get_fullname(input_json), doc)
--- utils/pdf_helper.py
+++ utils/pdf_helper.py
@@ -6,18 +6,22 @@ class PDF_Utils:
     WIDTH = 600
     HEIGHT = 400
 
-    def add_cover(name, dob):
+    def __init__(self):
+    # If you want to do any initial processing, add it here.
+        pass
+
+    def add_cover(self, name, dob):
         doc = pymupdf.Document()
         coverpage = doc.new_page(-1, PDF_Utils.WIDTH, PDF_Utils.HEIGHT)
         covertext_html = "<h1 style>Numerology Guide</h1><p>Report for: " + name + " <br>Date of Birth:" + dob + " <br><sub>Report Timestamp:" + str(datetime.datetime.now()) + "</sub> </p>"
         covertext_rect = pymupdf.Rect(50, 50, 450, 250)
-        coverpage.insert_html(covertext_rect, covertext_html )
+        coverpage.insert_htmlbox(covertext_rect, covertext_html )
         return doc
 
-    def add_page(doc):
+    def add_page(self, doc):
         page = doc.new_page(-1, PDF_Utils.WIDTH, PDF_Utils.HEIGHT)
 
-    def save_pdf(filename, doc):
-        print(filename)
-        doc.save( "Report_"+filename + ".pdf")
-    
+    def save_pdf(self, filename, doc):
+        filename = "Report for {}.pdf".format(filename)
+        print('SUCCESS: Report with file name \'' + filename + '\' saved')
+        doc.save( filename)
